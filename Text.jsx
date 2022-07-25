01d75fdafa9091e2a9db769edcafa0f8182e395e
bar064 <101471729+bar064@users.noreply.github.com>
2022-07-25 17:05:23 +0300

    first Uplaod

---
+++ Text.jsx
@@ -0,0 +1,114 @@
+const Text = () => {
+    return (
+      <>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil dolores
+        repellendus vero ut aliquam atque dolorum? Possimus nostrum non veritatis
+        hic necessitatibus reiciendis ullam maxime nobis. Accusamus, minus fugit.
+      </>
+    );
+  };
+  
+  export default Text;
+  
---
+++ sync Scorlling.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState, useCallback } from "react";
+import "./styles.css";
+import Text from "./Text";
+
+export default function App() {
+
+  // const [slavePercentage, setSlavePercentage] = useState(0);
+
+  const handleSlaveScroll = (e) => {
+     console.log(e.target.parentNode.parentNode.querySelectorAll(".slave"));
+    let slave = e.target;
+    let master = e.target.parentNode.querySelectorAll(".slave");
+    master.forEach(function(master) {
+        
+      let height = slave.scrollHeight - slave.clientHeight;
+      let percentage = (slave.scrollTop / height) * 100;
+      let masterHeight = master.scrollHeight - master.clientHeight;
+      // console.log(slave.scrollTop)
+      let newMasterPosition = (slave.scrollTop / height) * masterHeight;
+      // setSlavePercentage(percentage);
+      master.scroll({
+        top: newMasterPosition
+      });
+    });
+  };
+
+  // console.log("reload");
+  // console.log("master", masterPercentage);
+  // console.log("slave", slavePercentage);
+
+  return (
+    <div className="app">
+       <div className="app">
+      <div onScroll={handleSlaveScroll} className="slave">
+        "first"
+        <Text />
+      </div>
+      </div>
+      
+      <div className="app">
+      <div onScroll={handleSlaveScroll} className="slave">
+        <Text />
+      </div>
+      </div>
+  
+    </div>
+  );
+}
